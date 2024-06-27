9f40dca1346309f0e98073a2b5c1d4f29de658ad
NabilibnMahabub <mdnabil760@gmail.com>
2024-06-27 20:50:45 +0600

    new file:   assets/player/blue/0.png 	new file:   assets/player/blue/crauch1.png 	new file:   assets/player/blue/crauch2.png 	new file:   assets/player/red/0.png 	new file:   assets/player/red/crauch1.png 	new file:   assets/player/red/crauch2.png 	new file:   entitys/__pycache__/player.cpython-310.pyc 	modified:   entitys/player.py 	modified:   main.py

--- entitys/player.py
+++ entitys/player.py
@@ -1,6 +1,12 @@
 #importing libreraris
 import pygame
 
-x = 200
-y = 200
+class Player(pygame.sprite.Sprite):
+    def __init__(self ,x ,y ,scale):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load('assets/player/blue/0.png')
+        self.image = pygame.transform.scale(img,(int(img.get_width()*scale),int(img.get_height()*scale)))
+        self.rect = self.image.get_rect()
+        self.rect.center = (x,y)
+
 
