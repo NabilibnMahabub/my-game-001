bdb4536deb1fd7631cf412188227bd3d5e494442
NabilibnMahabub <mdnabil760@gmail.com>
2024-06-30 17:25:09 +0600

    modified:   assets/grenade/grenade_5.png 	modified:   entitys/__pycache__/grenade.cpython-310.pyc 	modified:   entitys/grenade.py

--- entitys/grenade.py
+++ entitys/grenade.py
@@ -14,22 +14,28 @@ class Grenade(pygame.sprite.Sprite):
         self.timer = 100
         self.speed = 15
         self.val_y = -11
+        self.frame_index = 0
         self.flip = False
         self.player = player
         self.enemy = enemy
+        self.animation = [ ]
+        self.update_time = pygame.time.get_ticks()
 
         img = pygame.image.load('assets/grenade/grenade_5.png').convert_alpha()
         sheet = spritesheet(img)
 
+        for i in range(3):
+            image = sheet.get_image(i,16,16,1,BLACK)
+            self.animation.append(image)
         
-        self.image = sheet.get_image(0,16,16,1,BLACK)
-        
+        self.image = self.animation[self.frame_index]
 
         self.rect = self.image.get_rect()
         self.rect.center = (x,y)
         self.direction = direction
     
     def update(self):
+        self.update_animation()
         dy = 0 
         #shoot bulet
         self.rect.x += self.direction *self.speed
@@ -61,7 +67,42 @@ class Grenade(pygame.sprite.Sprite):
         else:
             self.flip =False
         
-        """# Check collision with player
+        
+    def update_animation(self):
+        pass
+        ANIMATION_COOLDOWN = 200
+
+        self.image = self.animation[self.frame_index]
+
+        #game tick
+        if pygame.time.get_ticks() - self.update_time > ANIMATION_COOLDOWN:
+            self.update_time = pygame.time.get_ticks()
+            self.frame_index += 1
+        #looping the images
+        if self.frame_index == len(self.animation):
+            self.frame_index = len(self.animation) - 1
+        
+        
+
+    def draw(self,screen):
+        screen.blit(self.image, self.rect)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""# Check collision with player
         if pygame.sprite.spritecollide(self.player,grenade_group,False):
             if self.player.alive:
                 self.player.health -= 5
@@ -72,8 +113,3 @@ class Grenade(pygame.sprite.Sprite):
             if self.enemy.alive:
                 self.enemy.health -= 5
                 self.kill()"""
-        
-        
-
-    def draw(self,screen):
-        screen.blit(self.image, self.rect)
